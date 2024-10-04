e8bfd58c3b05bdb275b0ea2772390f105c122082
Beloin Sena <beloin.rodrigues@gmail.com>
2024-10-03 23:21:40 -0300

    Updated README.md and added initial command

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Current settings version: 0.0.1
 
-```JSON
+```lua
 {
   "Beloin/Launch.nvim",
   config = function()
--- lua/Launch/init.lua
+++ lua/Launch/init.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 function M.setup() end
-vim.api.nvim_create_user_command("Launch.nvim", function(opts)
+vim.api.nvim_create_user_command("Launchnvim", function(opts)
 	local m = require("Launch.configure_dap")
-  m.configure()
+	m.configure()
 end, {})
 
 return M
